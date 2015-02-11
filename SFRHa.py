15a2d2a932609a4f987f36cc05ddf64f606f27b9
Eduardo Alberto Duarte Lacerda <dhubax@gmail.com>
2015-02-11 01:09:28 +0100

    using califa_scripts now

--- SFRHa.py
+++ SFRHa.py
@@ -8,9 +8,11 @@
 #EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
 import numpy as np
 from pystarlight.util.base import StarlightBase
+from califa_scripts import create_dx, SFR_parametrize, \
+                           SFR_parametrize_trapz
 
-#useTrapz = True
-useTrapz = False
+useTrapz = True
+#useTrapz = False
 plot = True
 #plot = False
 outputImgSuffix = 'pdf'
@@ -66,48 +68,6 @@ bases = [ 'Padova1994.chab', 'Padova1994.salp', 'Padova2000.chab', 'Padova2000.s
 baseFile    = '/Users/lacerda/LOCAL/data/Base.bc03.h5'
 
 
-def create_dx(x):
-    dx          = np.zeros_like(x)
-    dx[1:]      = (x[1:] - x[:-1])/2.   # dl/2 from right neighbor
-    dx[:-1]     += dx[1:]               # dl/2 from left neighbor
-    dx[0]       = 2 * dx[0]
-    dx[-1]      = 2 * dx[-1]
-    return dx
-
-
-def SFR_parametrize(flux, wl, ages, max_age, useTrapz = False):
-    '''
-    Find the k parameter in the equation SFR = k [M_sun yr^-1] L(Halpha) [(10^8 L_sun)^-1]
-    
-    TODO: blablabla
-    
-    Nh__Zt is obtained for all t in AGES differently from Nh__Z, which consists in the number
-    of H-ionizing photons from MAX_AGE till today (t < MAX_AGE).
-    ''' 
-    from pystarlight.util.constants import L_sun, h, c, yr_sec
-    
-    cmInAA      = 1e-8          # cm / AA
-    lambda_Ha   = 6562.8        # Angstrom
-    mask_age    = ages <= max_age
-    
-    y = flux * wl * cmInAA * L_sun / (h * c)
-    
-    if useTrapz:
-        import scipy.integrate as spi
-
-        qh__Zt = np.trapz(y = y, x = wl, axis = 2) # 1 / Msol
-        Nh__Zt = spi.cumtrapz(y = qh__Zt, x = ages, initial = 0, axis = 1) * yr_sec
-        Nh__Z = np.trapz(y = qh__Zt[:, mask_age], x = ages[mask_age], axis = 1) * yr_sec
-    else:
-        qh__Zt = (y * create_dx(l)).sum(axis = 2)
-        Nh__Z = (qh__Zt[:, mask_age] * create_dx(ages[mask_age])).sum(axis = 1) * yr_sec
-        Nh__Zt = np.cumsum(qh__Zt * create_dx(ages), axis = 1) * yr_sec
-         
-    k_SFR__Z = 2.226 * lambda_Ha * L_sun * yr_sec / (Nh__Z * h * c) # M_sun / yr
-    
-    return qh__Zt, Nh__Zt, Nh__Z, k_SFR__Z
-
-
 if __name__ == '__main__':
     for i, b in enumerate(bases):
         base        = StarlightBase(baseFile, b, hdf5 = True)
@@ -115,12 +75,16 @@ if __name__ == '__main__':
         max_yr      = base.ageBase[-1]
         max_yr      = 1e7
         mask        = base.l_ssp <= 912         # Angstrom
-        mask_age    = base.ageBase <= max_yr
         
         f_ssp   = base.f_ssp[:,:,mask]
         l       = base.l_ssp[mask]
-    
-        qh__Zt, Nh__Zt, Nh__Z, k_SFR__Z = SFR_parametrize(f_ssp, l, base.ageBase, max_yr, useTrapz)
+        
+        if useTrapz:
+            SFRp = SFR_parametrize_trapz
+        else:
+            SFRp = SFR_parametrize
+        
+        qh__Zt, Nh__Zt, Nh__Z, k_SFR__Z = SFRp(f_ssp, l, base.ageBase, max_yr, useTrapz)
         
         print b + ':'
         for i, Z in enumerate(base.metBase):
@@ -140,16 +104,16 @@ if __name__ == '__main__':
             subax = add_subplot_axes(ax2, subpos)
     
             for iZ, Z in enumerate(base.metBase):
-                ax1.plot(np.log10(base.ageBase), Nh__Zt[iZ, :] / 1e60,
+                ax1.plot(np.ma.log10(base.ageBase), Nh__Zt[iZ, :] / 1e60,
                          c = plotConf__Z[iZ]['c'], lw = plotConf__Z[iZ]['lw'],  
                          label = r'Z $=\ %.2f Z_\odot$' % (Z / base.metBase[4]))
-                ax2.plot(np.log10(base.ageBase), Nh__Zt[iZ, :] / Nh__Zt[iZ, -1], 
+                ax2.plot(np.ma.log10(base.ageBase), Nh__Zt[iZ, :] / Nh__Zt[iZ, -1], 
                          c = plotConf__Z[iZ]['c'], lw = plotConf__Z[iZ]['lw'], 
                          label = r'Z $=\ %.2f Z_\odot$' % (Z / base.metBase[4]))
-                subax.plot(np.log10(base.ageBase), Nh__Zt[iZ, :] / Nh__Zt[iZ, -1], 
+                subax.plot(np.ma.log10(base.ageBase), Nh__Zt[iZ, :] / Nh__Zt[iZ, -1], 
                            c = plotConf__Z[iZ]['c'], lw = plotConf__Z[iZ]['lw'], 
                            label = r'Z $=\ %.2f Z_\odot$' % (Z / base.metBase[4]))
-                ax3.plot(np.log10(base.ageBase), np.log10(qh__Zt[iZ, :]),
+                ax3.plot(np.ma.log10(base.ageBase), np.ma.log10(qh__Zt[iZ, :]),
                          c = plotConf__Z[iZ]['c'], lw = plotConf__Z[iZ]['lw'], 
                          label = r'Z $=\ %.2f Z_\odot$' % (Z / base.metBase[4]))
                     
@@ -167,6 +131,21 @@ if __name__ == '__main__':
             subax.yaxis.set_minor_locator(MultipleLocator(0.05))
             subax.xaxis.grid(which='minor')
             subax.yaxis.grid(which='minor')
+            
+            ax1.xaxis.set_major_locator(MultipleLocator(1))
+            ax1.xaxis.set_minor_locator(MultipleLocator(0.25))
+            ax1.yaxis.set_major_locator(MultipleLocator(1))
+            ax1.yaxis.set_minor_locator(MultipleLocator(0.25))
+            ax1.xaxis.grid(which='major')
+            ax1.yaxis.grid(which='major')
+            
+            ax2.xaxis.set_major_locator(MultipleLocator(1))
+            ax2.xaxis.set_minor_locator(MultipleLocator(0.25))
+            ax2.yaxis.set_major_locator(MultipleLocator(0.25))
+            ax2.yaxis.set_minor_locator(MultipleLocator(0.05))
+            ax2.xaxis.grid(which='major')
+            ax2.yaxis.grid(which='major')
+            
             ax3.xaxis.set_major_locator(MultipleLocator(1))
             ax3.xaxis.set_minor_locator(MultipleLocator(0.5))
             ax3.yaxis.set_major_locator(MultipleLocator(2))
